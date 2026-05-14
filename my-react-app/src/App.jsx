734639b415d28eb59c4c6374c29682c55501fda5
Abhishek-Gharat <www.gharatabhi50@gmail.com>
2026-05-14 14:52:05 +0530

    Initial commit: React Vite application

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Signup from './pages/Signup'
+function App() {
+
+  return (
+    <>
+      <div>
+        <Signup />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ my-react-app/src/pages/Signup.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+function Signup() {
+  return (
+    <div className="signup-container">
+
+        <div className='sign-up-card'>
+
+      <h1>Signup</h1>
+      <input />
+      <input/>
+      <input/>
+
+        </div>
+    </div>
+  );
+}
+
+export default Signup;
