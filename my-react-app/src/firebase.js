b1b830a10a09a0e20b41f6af3fdf155d3c346326
Abhishek-Gharat <www.gharatabhi50@gmail.com>
2026-05-15 10:30:14 +0530

    user store in firebase

---
+++ my-react-app/src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD8L86KAfCrkrVLC3fNCHWXTnWaKuNFqek",
+  authDomain: "my-web-app-ef23f.firebaseapp.com",
+  projectId: "my-web-app-ef23f",
+  storageBucket: "my-web-app-ef23f.firebasestorage.app",
+  messagingSenderId: "378141226035",
+  appId: "1:378141226035:web:0c5155b70ff36d7ecd1f46",
+  measurementId: "G-Z5M74Y22D1"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
--- my-react-app/src/pages/Signup.jsx
+++ my-react-app/src/pages/Signup.jsx
@@ -1,17 +1,64 @@
-import React from 'react';
+import {useState} from 'react';
+import './Signup.css'
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 function Signup() {
+    const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+  const handleSubmit = async (e) => {
+    setEmail("")
+setPassword("")
+setConfirmPassword("")
+    setError("")
+    e.preventDefault();
+    if(!email || !password || !confirmPassword){
+        setError('All fields are required');
+        return;
+    }
+    
+    if(password !== confirmPassword){
+        setError('Passwords do not match');
+
+        return;
+    }
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(userCredential.user);
+    } catch (error) {
+      console.log(error.message);
+
+      setError(error.message);
+    }
+
+  }
+  console.log(email, password, confirmPassword);
   return (
+    <form onSubmit={handleSubmit}>
     <div className="signup-container">
 
         <div className='sign-up-card'>
-
+   
       <h1>Signup</h1>
-      <input />
-      <input/>
-      <input/>
+      <input type='email' placeholder='Email' value={email} onChange={(e)=>setEmail(e.target.value)}/>
+      <input type='password' placeholder='password' value={password} onChange={(e)=>setPassword(e.target.value)}/>
+      <input type='password' placeholder='confirm password' value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)}/>
+
+<h1>have an account?</h1>   
+<button type="submit">
+
+   Sign Up
+</button>
 
         </div>
+<p>{error}</p>
+             <p>{email}</p>
+<p>{password}</p>
+<p>{confirmPassword}</p>
     </div>
+    </form>
   );
 }
 
